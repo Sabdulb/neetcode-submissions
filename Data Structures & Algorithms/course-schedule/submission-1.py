af279baf390a05435fb4696766826935c6359f03
Salem <93056340+Sabdulb@users.noreply.github.com>
2026-04-08 22:56:39 -0400

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.py
@@ -0,0 +1,37 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
+        found = set()
+        mp = defaultdict(set)
+
+        for i,j in prerequisites:
+            mp[i].add(j)
+        
+        for i in range(numCourses):
+            if i not in mp:
+                found.add(i)
+        
+        def dfs(i, visit):
+
+            if i in visit:
+                return False
+            
+            if i in found:
+                return True
+            
+            visit.add(i)
+            for key in mp[i]:
+                if dfs(key, visit) == False:
+                    return False
+            
+            found.add(i)
+            return True
+        
+        print(found)
+        for i in range(numCourses):
+            if i not in found:
+                if dfs(i, set()) == False:
+                    return False
+        
+        return True
+            
