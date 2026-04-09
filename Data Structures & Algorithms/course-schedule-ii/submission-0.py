6ed570b456880f81d21af6285b4eb84e79be1609
Salem <93056340+Sabdulb@users.noreply.github.com>
2026-04-09 15:12:13 -0400

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.py
@@ -0,0 +1,41 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        
+        ret = []
+        found = set()
+        visit = set()
+        mp = defaultdict(list)
+
+        for i,j in prerequisites:
+            mp[i].append(j)
+        
+        def dfs(course, visit):
+
+            if course not in mp:
+                if course not in found:
+                    found.add(course)
+                    ret.append(course)
+                return True
+            
+            if course in visit:
+                return False
+
+            visit.add(course)
+            
+            for vals in mp[course]:
+
+                if dfs(vals, visit) == False:
+                    return False
+                
+            found.add(course)
+            ret.append(course)
+
+            return True
+        
+        for i in range(numCourses):
+            if dfs(i, set()) == False:
+                return []
+        
+        return ret
+                
+        
