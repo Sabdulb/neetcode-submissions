05696e0a0ce2d4ee817375d99fd3bcc3d7b85f73
Salem <93056340+Sabdulb@users.noreply.github.com>
2026-04-30 04:18:02 -0400

    Add: coin-change-ii - submission-5

---
+++ Data Structures & Algorithms/coin-change-ii/submission-5.py
@@ -0,0 +1,21 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        
+        dp = {}
+
+        def dfs(i,a):
+            if a == 0:
+                return 1
+            if i >= len(coins):
+                return 0
+            
+            if (i,a) in dp:
+                return dp[(i,a)]
+            
+            dp[(i,a)] = dfs(i + 1, a)
+            if a >= coins[i]:
+                dp[(i,a)] += dfs(i, a - coins[i])
+            
+            return dp[(i,a)]
+        
+        return dfs(0,amount)
